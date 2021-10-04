9cece09b5147e5a708334739dfe16fcddb217c29
Sam F <samfaz10@hotmail.com>
2021-10-04 13:20:00 -0400

    Done

--- Part 5 - Console Decisions/Program.cs
+++ Part 5 - Console Decisions/Program.cs
@@ -6,41 +6,138 @@ namespace Part_5___Console_Decisions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Part 1 - Stages");
+            Console.WriteLine("");
+
+            int Age;
+
             Console.WriteLine("Please Enter Your Age:");
-            int Age = Convert.ToInt32(Console.ReadLine());
 
-            if (Age >= 18)
+            if (Int32.TryParse(Console.ReadLine(), out Age))
             {
-                Console.WriteLine("You are an Adult");
+
+                if (Age >= 18)
+                {
+                    Console.WriteLine("You are an Adult!");
+                }
+
+                else if (Age <= 0)
+                {
+                    Console.WriteLine("Error: That is not a real age!");
+                }
+
+                else if (Age <= 5)
+                {
+                    Console.WriteLine("You are a Toddler!");
+                }
+
+                else if (Age <= 10)
+                {
+                    Console.WriteLine("You are a Child!");
+                }
+
+                else if (Age <= 12)
+                {
+                    Console.WriteLine("You are a Preteen!");
+                }
+
+                else if (Age > 12)
+                {
+                    Console.WriteLine("You are an Teen!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("An age is a NUMBER!!!!!");
             }
 
-            else if (Age <= 0)
+            Console.WriteLine();
+
+
+
+
+            Console.WriteLine("Part 2 - Hurricane");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Enter a category of a hurricane:");
+            Console.WriteLine("(Hurricanes range from category 1 - 5)");
+
+            int category;
+
+            category = Convert.ToInt32(Console.ReadLine());
+
+            switch (category)
             {
-                Console.WriteLine("Error: That is not a real age!");
+
+                case 1:
+                    Console.WriteLine("Category 1: 74-95mph or 64-82kt or 119-153km/hr");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Category 2: 96-110mph or 83-95kt or 154-177km/hr");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Category 3: 111-130mph or 96-113kt or 178-209km / hr");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Category 4: 131-155mph or 114-135kt or 210-249km/hr");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Category 5: Greater than 155mph or 138 kt or 249km/hr");
+                    break;
+
+                default:
+                    Console.WriteLine("You did not enter a hurricane category!!!");
+                    break;
+
             }
 
-            else if (Age <= 5)
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Part 3 - Random Divisible");
+            Console.WriteLine("");
+
+            Random generator = new Random();
+            int randomNum = generator.Next(2 , 6);
+            int userInput;
+            int divisible = 0;
+
+            Console.WriteLine("Please Enter a Number Higher Than " + randomNum + ":");
+
+            if (int.TryParse(Console.ReadLine(), out userInput))
             {
-                Console.WriteLine("You are a Toddler");
+                divisible = (userInput % randomNum);
             }
 
-            else if (Age <= 10)
+                if (userInput <= randomNum)
+                {
+                    Console.WriteLine("Error: You Entered a Number Lower Than " + randomNum);
+                }
+
+                else if (divisible == 0)
+                {
+                    Console.WriteLine("Divisible");
+                }
+
+                else if (divisible >= 1)
+                {
+                    Console.WriteLine("Not Divisible");
+                }
+
+            else
             {
-                Console.WriteLine("You are a Child");
+                Console.WriteLine("That is no number!");
             }
 
-            else if (Age <= 12)
-            {
-                Console.WriteLine("You are a Preteen");
-            }
 
-            else if (Age > 12)
-            {
-                Console.WriteLine("You are an Teen");
-            }
+            Console.ReadLine();
 
-            
-            
 
         }
     }
